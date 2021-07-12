71c8ef111e6e93829cc705a04648fc8d9436640b
adil chbada <nanoadil@gmail.com>
2021-07-12 10:31:04 +0100

    save code

--- lib/app/containers/app_home/lib/app/modules/home/views/home_view.dart
+++ lib/app/containers/app_home/lib/app/modules/home/views/home_view.dart
@@ -5,49 +5,82 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+  static final sidesColor = Colors.grey.shade100;
+  static const centerColor = Colors.white;
+  static const sidesFlex = 1;
+  static const centerFlex = 3;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.ac_unit),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.ac_unit),
+            onPressed: () {},
+          )
+        ],
+        title: Row(
+          children:  [
+            const ExpandedContainer(
+              flex: sidesFlex,
+              child: Text('start'),
+            ),
+            const ExpandedContainer(
+              flex: centerFlex,
+              child: Center(child: Text('center')),
+            ),
+            ExpandedContainer(
+              // color: sidesColor,
+              flex: sidesFlex,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children:const [
+                   Text('end'),
+                ],
+              ),
+            ),
+          ],
+        ),
+        // centerTitle: true,
       ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Expanded(
-            flex: 1,
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text('HomeView'),
-                centerTitle: true,
-              ),
-              // body:,
-            ),
+          ExpandedContainer(
+            color: sidesColor,
+            flex: sidesFlex,
+            child: const Text('start'),
           ),
-          Expanded(
-            flex: 2,
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text('HomeView'),
-                centerTitle: true,
-              ),
-              // body:,
-            ),
+          const ExpandedContainer(
+            color: centerColor,
+            flex: centerFlex,
+            child: Text('center'),
           ),
-          Expanded(
-            flex: 1,
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text('HomeView'),
-                centerTitle: true,
-              ),
-              // body:,
-            ),
+          ExpandedContainer(
+            color: sidesColor,
+            flex: sidesFlex,
+            child: const Text('end'),
           ),
         ],
       ),
     );
+  }
+}
+
+class ExpandedContainer extends StatelessWidget {
+  final Color? color;
+  final int flex;
+  final Widget child;
+
+  const ExpandedContainer({Key? key, required this.child, this.flex = 1, this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(flex: flex, child: Container(color: color, child: child));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Application',
       initialRoute: AppPages.initial,
       getPages: AppPages.routes,
